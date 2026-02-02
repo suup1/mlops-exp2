07d6d43a54b13a9edc60fdfc504fa28a10872d37
suup1 <suup1@users.noreply.github.com>
2026-02-03 02:18:02 +0530

    Add Azure ML CI pipeline for Exp-2

---
+++ src/train.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+preds = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, preds))
